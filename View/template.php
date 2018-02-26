93d71254f6a9f47900e60cb0dc7d9340c0a0c2f7
Yann Desdevises <ydesdev@hotmail.com>
2018-02-26 16:38:52 +0100

    Ajout d'un template d'erreur

---
+++ View/template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="Public/bootstrap/css/bootstrap.min.css">
+    <!--Optimal theme -->
+    <link rel="stylesheet" href="Public/bootstrap/css/bootstrap-theme.min.css">
+</head>
+
+<body>
+<?= $content ?>
+</body>
+</html>
